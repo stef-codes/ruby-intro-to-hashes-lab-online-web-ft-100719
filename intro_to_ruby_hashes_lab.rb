6519936debf81019e1bd4009196bed3d0bcb46ff
Stefanie Davis <davisstefanie2010@gmail.com>
2019-10-12 20:25:18 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -52,8 +52,8 @@ def monopoly_with_fourth_tier
 	    :pieces => 4, 
 	    :names => {
 	      :reading_railroad => {
-	        "mortgage_value" => "$100"
-	        ""
+	        "mortgage_value" => "$100",
+	        "pennsylvania_railroad" => "$200"
 	      }, 
 	      :pennsylvania_railroad => {}, 
 	      :b_and_o_railroad => {}, 
